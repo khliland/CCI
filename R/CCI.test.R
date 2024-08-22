bfe6e344c5399761836df5202d731d83e0a1b037
Christian Thorjussen <christianbern@gmail.com>
2024-08-22 11:48:16 +0200

    d

--- R/CCI.test.R
+++ R/CCI.test.R
@@ -20,6 +20,7 @@
 #' @param ... Additional arguments to pass to the \code{perm.test} function.
 #'
 #' @importFrom stats lm predict
+#' @importFrom dplyr %>%
 #'
 #' @return Invisibly returns the result of \code{perm.test}, which is an object of class 'CCI' containing the null distribution, observed test statistic, p-values, the machine learning model used, and the data.
 #' @export
